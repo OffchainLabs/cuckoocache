e7c8ad0370ac184ba9483ef775101d98ec0cb3fd
Ed Felten <ed@offchainlabs.com>
2024-02-14 06:03:39 -0500

    Count storage accesses in evaluation

--- evaluation/evaluation.go
+++ evaluation/evaluation.go
@@ -12,13 +12,15 @@ func EvaluateOnData[CacheKey cacheKeys.LocalNodeCacheKey](
 	onChainSize uint64,
 	localSize uint64,
 	accesses []CacheKey,
-) (uint64, uint64) { // (onChainHits, localHits)
-	onChain := onChainIndex.OpenOnChainCuckooTable(onChainStorage.NewMockOnChainStorage(), onChainSize)
+) (uint64, uint64, uint64, uint64) { // (onChainHits, localHits, storageReads, storageWrites)
+	storage := onChainStorage.NewMockOnChainStorage()
+	onChain := onChainIndex.OpenOnChainCuckooTable(storage, onChainSize)
 	onChain.Initialize(onChainSize)
 	cache := cuckoo_cache.NewLocalNodeCache[CacheKey](localSize, onChain, cacheBackingStore.NewMockBackingStore())
 
 	onChainHits := uint64(0)
 	localHits := uint64(0)
+	storageReadsBefore, storageWritesBefore := storage.(*onChainStorage.MockOnChainStorage).GetAccessCounts()
 	for _, key := range accesses {
 		if cuckoo_cache.IsInLocalNodeCache(cache, key) {
 			localHits++
@@ -29,5 +31,9 @@ func EvaluateOnData[CacheKey cacheKeys.LocalNodeCacheKey](
 		}
 	}
 
-	return onChainHits, localHits
+	storageReads, storageWrites := storage.(*onChainStorage.MockOnChainStorage).GetAccessCounts()
+	storageReads -= storageReadsBefore
+	storageWrites -= storageWritesBefore
+
+	return onChainHits, localHits, storageReads, storageWrites
 }
