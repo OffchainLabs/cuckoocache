9f5c2efdfa1f7fdd1821224300708b56e8f25164
Ed Felten <ed@offchainlabs.com>
2024-02-20 12:21:28 -0500

    Add slots to on-chain storage

--- onChainStorage/onchain_storage.go
+++ onChainStorage/onchain_storage.go
@@ -8,12 +8,31 @@ import "github.com/ethereum/go-ethereum/common"
 type OnChainStorage interface {
 	Get(location common.Hash) (common.Hash, error)
 	Set(location, value common.Hash) error
+	Slot(location common.Hash) OnChainStorageSlot
+}
+
+type OnChainStorageSlot interface {
+	Get() (common.Hash, error)
+	Set(value common.Hash) error
 }
 
 type MockOnChainStorage struct {
 	contents   map[common.Hash]common.Hash
 	readCount  uint64
 	writeCount uint64
+}
+
+type MockOnChainStorageSlot struct {
+	sto      *MockOnChainStorage
+	location common.Hash
+}
+
+func (m *MockOnChainStorageSlot) Get() (common.Hash, error) {
+	return m.sto.Get(m.location)
+}
+
+func (m MockOnChainStorageSlot) Set(value common.Hash) error {
+	return m.sto.Set(m.location, value)
 }
 
 func NewMockOnChainStorage() OnChainStorage {
@@ -38,6 +57,13 @@ func (m *MockOnChainStorage) Set(location, value common.Hash) error {
 		m.contents[location] = value
 	}
 	return nil
+}
+
+func (m *MockOnChainStorage) Slot(location common.Hash) OnChainStorageSlot {
+	return &MockOnChainStorageSlot{
+		sto:      m,
+		location: location,
+	}
 }
 
 func (m *MockOnChainStorage) GetAccessCounts() (uint64, uint64) {
